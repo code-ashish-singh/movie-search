b542e7e7ba75979bf0a00fbad061012f94bb9434
code-ashish-singh <aashish.singh.2707@gmail.com>
2026-04-14 17:35:58 +0530

    updated project changes

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
  
 let arr = []
 for(let i = 2026 ; i >=2005 ; i--){
-    let pushVari = ` <button onClick class="px-4 py-1 bg-blue-600 text-white font-semibold uppercase rounded cursor-pointer " >
+    let pushVari = ` <button onclick="buttonToSearch(this)" class="px-4 py-1 bg-blue-600 text-white font-semibold uppercase rounded cursor-pointer " >
                    ${i}
                  </button>`
     arr.push(pushVari)
@@ -56,4 +56,38 @@ searchBtn.addEventListener('click',()=>{
      })
     })
 })
+
+function buttonToSearch(e){
+      card_div.innerHTML = " "
+    // IMG_POSTER , TITLE , RANK,AKA , ACTORS , YEAR,
+
+
+    fetch(apiUrl+e.innerText).then(res=>res.json()).then((data)=>{
+     const movieDetails =    data.description  
+            
+             movieDetails.forEach((movies)=>{
+                
+              console.log(movies)
+                
+        card_div.innerHTML +=   ` 
+                     <div id="movie_box" class="h-100 rounded p-2 border-white border-1 overflow-hidden w-70">
+                      <div class="h-1/2" >
+                         <img class="h-full w-full object-cover" src=${movies['#IMG_POSTER']} alt="">
+                      </div>
+                      <div class="flex flex-col gap-1 mt-3 font-semibold text-white ">
+                        <p >Titile : ${movies['#TITLE']}</p>
+                        <p>ACTORS: ${movies['#ACTORS']}</p>
+                        <p>YEAR: ${movies['#YEAR']} </p>
+                        <p>RANK : ${movies['#RANK']} </p>
+                        <div class="text-right">
+                           <button class="px-4 py-1 bg-blue-600 text-white font-semibold  rounded cursor-pointer " >
+                    Watch Now
+                 </button>
+                        </div>
+                      </div>        
+                </div>
+        `
+     })
+    })
+}
 
